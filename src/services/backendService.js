ec44921ebe8e30061058e0b1654e3585ad810e8f
Dennis Olsen <dennan_lol@hotmail.com>
2025-09-24 11:53:16 +0200

    Fixed back end api calling

--- src/services/backendService.js
+++ src/services/backendService.js
@@ -1,16 +1,20 @@
+const todoAPIEndpoint = "http://localhost:8080/api/chatBot/ask";
+const todoAPIEndpoint2 = "http://localhost:5173/api/chatBot/ask";
+
 import axios from "axios";
 
 export async function callAiChatbot(question, conversationId) {
     console.log(`### Starting to call ai chatbot...`);
 
     try {
-        const response = await axios.get("/api/chatBot/ask", 
+        const response = await axios.get(todoAPIEndpoint, 
             {  
                 params: { 
                     question: question, 
                     conversationId: conversationId 
                 }
             });
+        console.log("Response from AI assistant successful.", response.data);
         return response.data;
     } catch (error) {
         console.error("Error calling AI assistant:", error);
